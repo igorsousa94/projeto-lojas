82863224aaa885db5576b9775ae2afdf481f63b6
C1310264 Marcelo Cesar Dolacio <c1310264@interno.bb.com.br>
2021-09-10 17:17:40 -0300

    recuperando variáveis da requisição login

--- Projeto Lojas/PerfilViewController.swift
+++ Projeto Lojas/PerfilViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class PerfilViewController: UIViewController {
     
+    @IBOutlet weak var nome: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var telefone: UILabel!
     @IBOutlet weak var quadradoBranco: UIView!
     var teste:Usuario? = nil;
     
@@ -18,5 +21,10 @@ class PerfilViewController: UIViewController {
         var mainViewController:LoginViewController?
                print("CHEGUEI NA TELA DE PERFIL")
                print(self.teste)
+               print(self.teste?.nome)
+
+        self.nome.text = self.teste?.nome
+        self.email.text = self.teste?.email
+        self.telefone.text = self.teste?.telefone
     }  
 }
